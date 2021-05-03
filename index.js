97de4f1d2930f488b1707298754addb7ac215456
Keerbi Liu <kirby.liu12@gmail.com>
2021-05-03 17:46:43 +1000

    initated a .json, installed inquirer, and added prompts as per assignment criteria.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Run the npm init -y then installed inquirer, npm i inquirer. */
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -5,9 +7,49 @@ inquirer
     .prompt([
         {
             type: "input",
-            name: "name",
-            message: "Project name",
-            
-        }
+            name: "Project title",
+            message: "What is your project title?:",
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "A short description about your project:",
+        },
+        {
+            type: "input",
+            name: "Table of contents",
+            message: "Table of contents for your headings:",
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "Project installation:",
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "Project usage:",
+        },
+        {
+            type: "input",
+            name: "License",
+            message: "Project license:",
+        },
+        {
+            type: "input",
+            name: "Contributing",
+            message: "Project contributions:",
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: "Project tests:",
+        },
+        {
+            type: "input",
+            name: "Questions",
+            message: "Questions on the project:",
+        },
     ]
 )
+
